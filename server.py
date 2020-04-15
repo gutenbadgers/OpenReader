e72a341c971b864d48e97b959197a88df389228f
Gabriel Kulp <gabrielkulp@gmail.com>
2020-04-14 20:20:22 -0700

    Added API usage example

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from flask import Flask
+import urllib.request, json 
+
 app = Flask(__name__)
+
+# https://github.com/garethbjohnson/gutendex#api
+with urllib.request.urlopen("http://gutendex.com/books/?search=dickens%20great") as url:
+	data = json.loads(url.read().decode())
+	print(data)
 
 @app.route('/')
 def hello_world():
