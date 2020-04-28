0cb5a611caf722722a8b0248ba081a38056bb9d5
Gabriel Kulp <gabrielkulp@gmail.com>
2020-04-28 16:26:13 -0700

    Improved form handling and unified search templates

--- app.py
+++ app.py
@@ -1,75 +1,78 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from collections import namedtuple
 import urllib.request, json
+#from collections import namedtuple
 
 app = Flask(__name__)
 
 # https://github.com/garethbjohnson/gutendex#api
 catalog = "http://gutendex.com/books"
 
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
-def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
+# unused utility function to turn API result into an object
+#def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+#def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
 
 def readCatalog(route):
 	url = catalog + route
 	with urllib.request.urlopen(url) as req:
 		return json.loads(req.read().decode())
 
+
 @app.route("/")
 @app.route("/index")
 def hello():
 	return render_template("index.html")
 
+
 @app.route("/login")
 def login():
-	return render_template("wip.html")
+	abort(501) # server error: unimplemented
+
 
 @app.route("/bookshelf")
 def bookshelf():
-	return render_template("wip.html")
+	abort(501) # server error: unimplemented
+
 
 @app.route("/book/<int:id>")
 def book(id):
 	book = readCatalog("/{}".format(id))
 	title = book["title"]
-
 	bookURL = "https://www.gutenberg.org/files/{0}/{0}-h/{0}-h.htm".format(id)
-	print(bookURL);
-
 	return render_template("bookInfo.html", **locals())
+
 
 @app.route("/book/<int:id>/read")
 def readBook(id):
 	bookURL = "https://www.gutenberg.org/files/{0}/{0}-h/{0}-h.htm".format(id)
-	print(bookURL);
-
 	website = urllib.request.urlopen(bookURL)
-	html = website.read()
-
-	return html
+	return website.read()
 
 
 @app.route('/search', methods=['GET'])
-def searchGet():
+def searchGET():
 	return render_template("search.html", title="Search")
 
+
 @app.route('/search', methods=['POST'])
-def search():
-	# Depending on which form is sent to this route, redirect to different routes
-	print(request.form.get("terms"))
+def searchPOST():
+	url = ""
 	if (request.form.get('searchType') == "title"):
 		url = "/search/title=" + urllib.parse.quote(request.form.get('terms'))
-		return redirect(url, code=302)
 	elif (request.form.get('searchType') == "author"):
 		url = "/search/author=" + urllib.parse.quote(request.form.get('terms'))
-		return redirect(url, code=302)
 	elif (request.form.get('searchType') == "category"):
 		url = "/search/category=" + urllib.parse.quote(request.form.get('terms'))
-		return redirect(url, code=302)
-	# no else. It won'd do anything unless they pick one.
+
+	if url:
+		return redirect(url, code=303)
+	else:
+		abort(400) # client error: invalid form
+
 
 @app.route("/search/title=<string:terms>")
-def searchResultsTitle(terms):
+def searchTitle(terms):
 	results = readCatalog("?search=" + urllib.parse.quote(terms))["results"]
 	title = "Search results"
 	searchType = "title"
@@ -77,31 +80,34 @@ def searchResultsTitle(terms):
 	# refine results to only include title matches
 	books = [x for x in results if terms.lower() in x["title"].lower()]
 
-	return render_template("searchResults.html", **locals())
+	return render_template("search.html", **locals())
+
 
 @app.route("/search/author=<string:terms>")
-def searchResultsAuthor(terms):
+def searchAuthor(terms):
 	results = readCatalog("?search=" + urllib.parse.quote(terms))["results"]
 	title = "Search results"
 	searchType = "author"
-	books = []
 
 	# refine results to only include author matches
+	books = []
 	for book in results:
 		if (book["authors"]):
 			for author in book["authors"]:
 				if (terms.lower() in author["name"].lower()):
 					books.append(book)
 
-	return render_template("searchResults.html", **locals())
+	return render_template("search.html", **locals())
+
 
 @app.route("/search/category=<string:terms>")
-def searchResultsCategory(terms):
+def searchCategory(terms):
 	books = readCatalog("?topic=" + urllib.parse.quote(terms))["results"]
 	title = "Search results"
 	searchType = "category"
 
-	return render_template("searchResults.html", **locals())
+	return render_template("search.html", **locals())
+
 
 if __name__ == "__main__":
 	app.run()
