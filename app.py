926232ba7f5603db4dc59b0e1b8c6453dc09d7c1
Gabriel Kulp <gabrielkulp@gmail.com>
2020-04-29 10:41:11 -0700

    Factored out PG requests

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, redirect, render_template, request, abort
 import urllib.request, json
 
 app = Flask(__name__)
 
 
 # https://github.com/garethbjohnson/gutendex#api
-catalog = "http://gutendex.com/books"
-
-
 def readCatalog(route):
-	url = catalog + route
+	url = "http://gutendex.com/books" + route
 	with urllib.request.urlopen(url) as req:
 		return json.loads(req.read().decode())
+
+
+# https://www.gutenberg.org/wiki/Gutenberg:Information_About_Robot_Access_to_our_Pages
+def readPG(route):
+	url = "https://www.gutenberg.org" + route
+	with urllib.request.urlopen(url) as req:
+		return req.read()
 
 
 @app.route("/")
@@ -31,24 +35,27 @@ def bookshelf():
 
 
 @app.route("/book/<int:id>")
-def book(id):
+def bookInfo(id):
 	book = readCatalog("/{}".format(id))
 	title = "Info: " + book["title"]
 	return render_template("bookInfo.html", **locals())
 
 
 @app.route("/book/<int:id>/read")
-def readBook(id):
-	bookURL = "https://www.gutenberg.org/files/{0}/{0}-h/{0}-h.htm".format(id)
-	with urllib.request.urlopen(bookURL) as req:
-		return req.read()
+def bookRead(id):
+	return readPG("/files/{0}/{0}-h/{0}-h.htm".format(id))
 
 
+# verified valid sizes: small, medium
+@app.route("/book/<int:id>/cover/<string:size>")
+def bookCover(id, size):
+	return readPG("/cache/epub/{0}/pg{0}.cover.{1}.jpg".format(id, size))
+
+
+# images referenced in book content
 @app.route("/book/<int:id>/images/<string:imageName>")
 def bookImage(id, imageName):
-	imageURL = "https://www.gutenberg.org/files/{0}/{0}-h/images/{1}".format(id, imageName)
-	with urllib.request.urlopen(imageURL) as req:
-		return req.read()
+	return readPG("/files/{0}/{0}-h/images/{1}".format(id, imageName))
 
 
 @app.route('/search', methods=['GET'])
@@ -62,7 +69,7 @@ def searchPOST():
 	url = ""
 
 	if searchType in ["title", "author", "category"]:
-		url = "/search/{}=".format(searchType)
+		url = "/search/{}/".format(searchType)
 	else:
 		abort(400) # client error: invalid form
 
@@ -70,23 +77,21 @@ def searchPOST():
 	return redirect(url, code=303)
 
 
-@app.route("/search/title=<string:terms>")
+@app.route("/search/title/<string:terms>")
 def searchTitle(terms):
 	results = readCatalog("?search=" + urllib.parse.quote(terms))["results"]
-	title = "Search results"
-	searchType = "title"
 
 	# refine results to only include title matches
 	books = [x for x in results if terms.lower() in x["title"].lower()]
 
+	title = "Search results"
+	searchType = "title"
 	return render_template("search.html", **locals())
 
 
-@app.route("/search/author=<string:terms>")
+@app.route("/search/author/<string:terms>")
 def searchAuthor(terms):
 	results = readCatalog("?search=" + urllib.parse.quote(terms))["results"]
-	title = "Search results"
-	searchType = "author"
 
 	# refine results to only include author matches
 	books = []
@@ -96,15 +101,17 @@ def searchAuthor(terms):
 				if (terms.lower() in author["name"].lower()):
 					books.append(book)
 
+	title = "Search results"
+	searchType = "author"
 	return render_template("search.html", **locals())
 
 
-@app.route("/search/category=<string:terms>")
+@app.route("/search/category/<string:terms>")
 def searchCategory(terms):
 	books = readCatalog("?topic=" + urllib.parse.quote(terms))["results"]
+
 	title = "Search results"
 	searchType = "category"
-
 	return render_template("search.html", **locals())
 
 
