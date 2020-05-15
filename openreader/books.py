172aba5ea8eb1d2114a14a62021a82554d1b7dfc
Gabriel Kulp <gabrielkulp@gmail.com>
2020-05-15 14:07:26 -0700

    Factored API access into catalog.py

--- openreader/books.py
+++ openreader/books.py
@@ -8,23 +8,9 @@ from werkzeug.exceptions import abort
 
 from openreader.auth import login_required
 from openreader.db import get_db
+import openreader.catalog as catalog
 
-bp = Blueprint('books', __name__)
-
-
-# https://github.com/garethbjohnson/gutendex#api
-def readCatalog(route):
-	url = "http://gutendex.com/books" + route
-	with urllib.request.urlopen(url) as req:
-		return json.loads(req.read().decode())
-
-
-# https://www.gutenberg.org/wiki/Gutenberg:Information_About_Robot_Access_to_our_Pages
-def readPG(route):
-	url = "https://www.gutenberg.org" + route
-	with urllib.request.urlopen(url) as req:
-		return req.read()
-
+bp = Blueprint("books", __name__, static_folder="static")
 
 @bp.route("/")
 def index():
@@ -40,7 +26,7 @@ def bookshelf():
 			"SELECT book_id FROM bookshelf WHERE user_id = ?",
 			(g.user["id"],)
 		).fetchall()
-		books = [readCatalog("/{}".format(b["book_id"])) for b in book_ids]
+		books = [catalog.get_info(b["book_id"]) for b in book_ids]
 		return render_template("bookshelf.html", books=books)
 	
 	action = request.form.get("action")
@@ -74,7 +60,7 @@ def bookshelf():
 @bp.route("/book/<int:id>")
 def info(id):
 	db = get_db()
-	res = readCatalog("/{}".format(id))
+	res = catalog.get_info(id)
 	logged_in = g.user and g.user["id"]
 	bookmarked = None
 
@@ -91,23 +77,22 @@ def info(id):
 
 @bp.route("/book/<int:id>/read")
 def read(id):
-	return readPG("/files/{0}/{0}-h/{0}-h.htm".format(id))
+	return catalog.get_content(id)
 
 
-# verified valid sizes: small, medium
 @bp.route("/book/<int:id>/cover/<string:size>")
 def cover(id, size):
-	try:
-		return readPG("/cache/epub/{0}/pg{0}.cover.{1}.jpg".format(id, size))
-	except urllib.error.HTTPError:
+	cover = catalog.get_cover(id, size)
+	if not cover:
 		img = 'noCover{}.jpg'.format(size.capitalize())
 		return bp.send_static_file(img)
+	return cover
 
 
 # images referenced in book content
 @bp.route("/book/<int:id>/images/<string:imageName>")
 def image(id, imageName):
-	return readPG("/files/{0}/{0}-h/images/{1}".format(id, imageName))
+	return catalog.get_content_image(id, imageName)
 
 
 @bp.route("/search", methods=["GET", "POST"])
@@ -116,46 +101,31 @@ def search():
 		return render_template("search.html")
 
 	searchType = request.form.get("searchType")
-	url = ""
 
-	if searchType in ["title", "author", "category"]:
-		url = "/search/{}/".format(searchType)
-	else:
+	if searchType not in ["title", "author", "category"]:
 		abort(400) # client error: invalid form
 
+	url = "/search/{}/".format(searchType)
 	url += urllib.parse.quote(request.form.get("terms"))
 	return redirect(url, code=303)
 
 
 @bp.route("/search/title/<string:terms>")
 def searchTitle(terms):
-	results = readCatalog("?search=" + urllib.parse.quote(terms))["results"]
-
-	# refine results to only include title matches
-	books = [x for x in results if terms.lower() in x["title"].lower()]
+	books = catalog.search("title", terms)
 	searchType = "title"
 	return render_template("search.html", **locals())
 
 
 @bp.route("/search/author/<string:terms>")
 def searchAuthor(terms):
-	results = readCatalog("?search=" + urllib.parse.quote(terms))["results"]
-
-	# refine results to only include author matches
-	books = []
-	for book in results:
-		if (book["authors"]):
-			for author in book["authors"]:
-				if (terms.lower() in author["name"].lower()):
-					books.append(book)
-
+	books = catalog.search("author", terms)	
 	searchType = "author"
 	return render_template("search.html", **locals())
 
 
 @bp.route("/search/category/<string:terms>")
 def searchCategory(terms):
-	books = readCatalog("?topic=" + urllib.parse.quote(terms))["results"]
-
+	books = catalog.search("category", terms)
 	searchType = "category"
 	return render_template("search.html", **locals())
---
+++ openreader/catalog.py
@@ -0,0 +1,59 @@
+import urllib.request, urllib.error
+import json
+
+# https://github.com/garethbjohnson/gutendex#api
+def _read_catalog(route):
+	url = "http://gutendex.com/books" + route
+	try:
+		with urllib.request.urlopen(url) as req:
+			return json.loads(req.read().decode())
+	except urllib.error.HTTPError:
+		return None
+
+
+# https://www.gutenberg.org/wiki/Gutenberg:Information_About_Robot_Access_to_our_Pages
+def _read_PG(route):
+	url = "https://www.gutenberg.org" + route
+	try:
+		with urllib.request.urlopen(url) as req:
+			return req.read()
+	except urllib.error.HTTPError:
+		return None
+
+
+# return book metadata
+def get_info(id):
+	return _read_catalog("/{}".format(id))
+
+
+# return book cover. verified valid sizes: small, medium
+def get_cover(id, size):
+	return _read_PG("/cache/epub/{0}/pg{0}.cover.{1}.jpg".format(id, size))
+
+
+# return book content
+def get_content(id):
+	return _read_PG("/files/{0}/{0}-h/{0}-h.htm".format(id))
+
+
+# return images referenced in book content
+def get_content_image(id, filename):
+	return _read_PG("/files/{0}/{0}-h/images/{1}".format(id, filename))
+
+
+# return a list of book metadata
+# Not: title and author search types artificially limit results to matches
+def search(type, terms):
+	if type == "title":
+		results = _read_catalog("?search=" + urllib.parse.quote(terms))["results"]
+		return [x for x in results if terms.lower() in x["title"].lower()]
+	elif type == "author":
+		results = _read_catalog("?search=" + urllib.parse.quote(terms))["results"]
+		return [b for b in results if b["authors"] and
+			True in (terms.lower() in a["name"].lower() for a in b["authors"])
+		]
+	elif type == "category":
+		return _read_catalog("?topic=" + urllib.parse.quote(terms))["results"]
+	else:
+		return None
+
